0d389ae37759ddf5e612fad869c86e4d383a13ca
Shubham Shrestha <shrestha.subham.60@gmail.com>
2025-08-07 11:58:52 +0545

    Fixed:Padding

--- client/src/components/features/shared/textblock/TextBlock.tsx
+++ client/src/components/features/shared/textblock/TextBlock.tsx
@@ -7,7 +7,7 @@ interface TextBlockProps {
 
 const TextBlock: React.FC<TextBlockProps> = ({ heading, subheading }) => {
   return (
-    <div className="w-full max-w-screen-xl px-4 py-4 flex flex-col justify-center items-start">
+    <div className="w-full max-w-screen-xl px-0 py-4 flex flex-col justify-center items-start">
       <h2 className="font-poppins font-semibold text-[24px] leading-[36px] text-black">
         {heading}
       </h2>
