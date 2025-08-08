eccf086bef1a6e97278ba663745a606f3945529f
ashim <magarashim69086@gmail.com>
2025-08-08 11:39:57 +0545

    merge:merging into test branch

--- client/src/app/(routes)/page.tsx
+++ client/src/app/(routes)/page.tsx
@@ -1,13 +1,9 @@
-import SliderSection from "@/components/features/home/Sliders/SliderSection";
-import HomeProductsComponent from "@/components/features/shared/cards/ProductsContainer";
+
 
 export default function Home() {
   return (
     <div className="">
       <div className="mx-4">
-
-        <SliderSection />
-        <HomeProductsComponent title="This Week’s Special Offers" subTitle="Don’t Miss Out — Limited-Time Deals Just for You!" link="babycake"/>
       </div>
     </div>
   );
