dad92bbb1b2c1339110c31ae3ea18bb8cbed0248
ashim <magarashim69086@gmail.com>
2025-08-08 16:30:20 +0545

    ui:product container gap increased

--- client/src/components/features/shared/cards/ProductsContainer.tsx
+++ client/src/components/features/shared/cards/ProductsContainer.tsx
@@ -48,7 +48,7 @@ const HomeProductsComponent: React.FC<IProps> = ({ title, subTitle, link, data }
                 {/* Product Grid */}
                 <div
                     className="
-          grid gap-16 
+          grid gap-22 
           grid-cols-1
           sm:grid-cols-2
           md:grid-cols-3
